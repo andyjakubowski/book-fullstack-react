aac7de9f1c5df9c95b5bf9513c5d2a0e7976e616
Andy Jakubowski <hello@andyjakubowski.com>
2020-04-22 09:24:23 +0200

    Add cursor-based pagination with GraphQL

--- graphql-server/server.js
+++ graphql-server/server.js
@@ -33,26 +33,7 @@ const RootQuery = new GraphQLObjectType({
         }
       },
       resolve(source, args, context, info) {
-        let includeFriends = false;
-        const selectionFragments = info.fieldASTs[0].selectionSet.selections;
-        const userSelections = selectionFragments.filter((selection) => {
-          return selection.kind === 'InlineFragment' 
-            && selection.typeCondition.name.value === 'User';
-        });
-
-        userSelections.forEach((selection) => {
-          selection.selectionSet.selections.forEach((innerSelection) => {
-            if (innerSelection.name.value === 'friends') {
-              includeFriends = true;
-            }
-          });
-        });
-
-        if (includeFriends) {
-          return loaders.getUserNodeWithFriends(args.id);
-        } else {
-          return loaders.getNodeById(args.id);
-        }
+        return loaders.getNodeById(args.id);
       }
     },
     viewer: {
